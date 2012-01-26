cdaf90dc46bd6f6886aa854e5f57567ef0e4ab70
Adi <Adi@AdiLaptop>
2012-01-26 09:34:23 +0100

    poprawka

--- src/Test.java
+++ src/Test.java
@@ -13,11 +13,11 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		Random random = new Random();
 		
-		for (int fileNr = 2; fileNr <= 2; fileNr++) {
+		for (int fileNr = 1; fileNr <= 9; fileNr++) {
 			Parser parser = new ITCParser("solutions/competition0" + Integer.toString(fileNr) + ".tim");
 			Constraints constraints = parser.parse();
 		
-			Timetable best = Evolution.run(30, 40, constraints, fileNr, 0.05, 0.5, 0.03);
+			Timetable best = Evolution.run(30, 60, constraints, fileNr, 0.05, 0.95, 0.1);
 			System.out.println("\n\nWynik końcowy: " + Integer.toString(best.penalty.intValue()));
 			System.out.println("\n\n\n\n***************************************\n\n");
 		}
